28731bd4b11da4770e0b27f7df9b8b809f04a70c
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2023-10-19 00:06:04 -0400

    Update for MHW 15.20.00

--- src/wml/game_address.h
+++ src/wml/game_address.h
@@ -1,16 +1,17 @@
 #pragma once
 #include <cstdint>
 
-// All of the addresses in this file are hard-coded to 15.11.01 version of the
-// game client The game hasn't been updated in 2+ years at the time of writing
-// this, so there doesn't seem to be any benefit to trying to find the addresses
-// dynamically.
+// As this game updates so infrequently (e.g. 2 year gap between 15.11.01 -> 15.20.00),
+// we hard-code the addresses in this file instead of attempting to dynamically locate
+// them at runtime based on AOBs or other patterns.
 
 namespace WML::Game::Address {
+// MHW 15.20.00:
 const uint64_t IMAGE_BASE = 0x140000000;
-const uint64_t PROCESS_OEP = IMAGE_BASE + 0x279a918;
-const uint64_t PROCESS_SECURITY_COOKIE = IMAGE_BASE + 0x4c56b88;
-const uint64_t SECURITY_COOKIE_INIT_GETTIME_RET = IMAGE_BASE + 0x279b592;
-const uint64_t SCRT_COMMON_MAIN_SEH = IMAGE_BASE + 0x279a7a4;
-const uint64_t WINMAIN = IMAGE_BASE + 0x13b5110;
+const uint64_t PROCESS_OEP = IMAGE_BASE + 0x2741668;
+const uint64_t PROCESS_SECURITY_COOKIE = IMAGE_BASE + 0x4bf4be8;
+const uint64_t SECURITY_COOKIE_INIT_GETTIME_RET = IMAGE_BASE + 0x27422e2;
+const uint64_t SCRT_COMMON_MAIN_SEH = IMAGE_BASE + 0x27414f4;
+const uint64_t WINMAIN = IMAGE_BASE + 0x13a4c00;
+
 }  // namespace WML::Game::Address
